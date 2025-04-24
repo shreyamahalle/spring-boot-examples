bb6a6e4e4fbc08d3d1d4a40b41020df3860354ce
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-24 20:53:58 +0530

    changes added

--- hello-world/src/main/java/com/shreya/spring/HelloWorldApplication.java
+++ hello-world/src/main/java/com/shreya/spring/HelloWorldApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldApplication.class, args);
 
 		System.out.println("Hello World!!!!!!");
