1ef5a3ce841a445e74712f4bca955a3d3c4a2e16
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-24 20:32:21 +0530

    changes added

---
+++ spring-boot-logging/src/main/java/com/shreya/example/SpringBootLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingApplication.class, args);
+	}
+
+}
