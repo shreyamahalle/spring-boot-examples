99716c7dea9ad4bebaa118d41ffceeecd5459b3a
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-24 12:08:44 +0530

    changes added

--- hello-world/src/main/java/com/shreya/spring/HelloWorldApplication.java
+++ hello-world/src/main/java/com/shreya/spring/HelloWorldApplication.java
@@ -7,6 +7,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+
 		System.out.println("Hello World!!!!!!");
 	}
 
