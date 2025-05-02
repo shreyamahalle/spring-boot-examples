1437848f09baa2d11f3e90e0b1e7e59b39e47368
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-02 11:55:28 +0530

    changes added

---
+++ student-management-practice/src/main/java/com/shreya/spring/StudentManagementPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementPracticeApplication.class, args);
+	}
+
+}
