fb5e679a9befa9a53d460331f1d9c26298bcfd24
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-05 19:50:24 +0530

    application runner code added

---
+++ spring-boot-application-runner/src/main/java/com/shreya/practice/SpringBootApplicationRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationRunnerApplication.class, args);
+	}
+
+}
