4f34059244a3628d91adff2ada0f641af7b31da4
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-06 12:49:38 +0530

    application runner practice example added

--- spring-boot-application-runner/src/main/java/com/shreya/practice/SpringBootApplicationRunnerApplication.java
+++ spring-boot-application-runner/src/main/java/com/shreya/practice/SpringBootApplicationRunnerApplication.java
@@ -1,13 +1,25 @@
 package com.shreya.practice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApplicationRunnerApplication {
+public class SpringBootApplicationRunnerApplication implements ApplicationRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootApplicationRunnerApplication.class);
+
+	@Override
+	public void run(final ApplicationArguments args) throws Exception {
+		log.info("Total NonOptionArgs: {}", args.getNonOptionArgs().size());
+		args.getNonOptionArgs().forEach( log::info);
+		log.info("Total OptionArgs: {}", args.getOptionNames().size());
+		args.getOptionNames().forEach(name -> log.info("{} = {}",name, args.getOptionValues(name)));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplicationRunnerApplication.class, args);
 	}
-
 }
