d5c7e41f6af504eeb27646f65ec01162590ec01c
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-07 13:01:55 +0530

    commandline runner example added

---
+++ spring-boot-commandline-runner/src/main/java/com/shreya/spring/SpringBootCommandlineRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommandlineRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommandlineRunnerApplication.class, args);
+	}
+
+}
