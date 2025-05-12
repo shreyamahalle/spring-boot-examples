00386cab7df43984093315d2793c346741a95b61
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-12 11:20:06 +0530

    changes added

--- spring-boot-filters/src/main/java/com/shreya/spring/SpringBootFiltersApplication.java
+++ spring-boot-filters/src/main/java/com/shreya/spring/SpringBootFiltersApplication.java
@@ -1,13 +1,26 @@
 package com.shreya.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringBootFiltersApplication {
+public class SpringBootFiltersApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootFiltersApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFiltersApplication.class, args);
 	}
 
+	public void run(String... args) {
+		if (0 == args.length)
+			log.debug("No arguments are provided to this program");
+		else
+			for (String arg : args)
+				log.debug("Argument received: {} ", arg);
+	}
 }
---
+++ spring-boot-filters/src/test/java/com/shreya/spring/controller/TrainingServiceControllerTests.java
@@ -0,0 +1,4 @@
+package com.shreya.spring.controller;
+
+public class TrainingServiceControllerTests {
+}
