d239d8478b6c5a957660ff6c40328056f7d23e70
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-12 11:24:42 +0530

    domain added

--- spring-boot-filters/src/main/java/com/shreya/spring/domain/Training.java
+++ spring-boot-filters/src/main/java/com/shreya/spring/domain/Training.java
@@ -1,5 +1,16 @@
 package com.shreya.spring.domain;
 
-public class Training
-{
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Training {
+    private int id;
+    private String name;
+    private int duration;
+    private String prerequisite;
+
 }
