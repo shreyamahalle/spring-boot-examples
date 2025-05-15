81d549b4a36f5073c417e1064862dcf696361056
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-15 11:24:44 +0530

    internationalization project added

---
+++ spring-boot-internationalization/src/test/java/com/shreya/internationalization/SpringBootInternationalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.internationalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInternationalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
