98c356233a1d68b537258431ad1ac3719353e2a4
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-20 17:56:23 +0530

    updated code

--- application-runner-demo/src/main/java/com/shreya/runner/ApplicationRunnerDemoApplication.java
+++ application-runner-demo/src/main/java/com/shreya/runner/ApplicationRunnerDemoApplication.java
@@ -1,12 +1,26 @@
 package com.shreya.runner;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+
 @SpringBootApplication
-public class ApplicationRunnerDemoApplication {
+public class ApplicationRunnerDemoApplication implements ApplicationRunner {
+
+	private Logger log = LoggerFactory.getLogger(ApplicationRunnerDemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationRunnerDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("Total non-option arguments {}",args.getNonOptionArgs());
+
+
 	}
 }
