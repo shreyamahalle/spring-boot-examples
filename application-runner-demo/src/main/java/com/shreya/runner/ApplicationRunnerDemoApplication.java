c0aee3af4cbacb975917257db58f6e720edf8d1e
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-20 15:09:40 +0530

    application-runner example added

---
+++ application-runner-demo/src/main/java/com/shreya/runner/ApplicationRunnerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.shreya.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunnerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationRunnerDemoApplication.class, args);
+	}
+}
---
+++ application-runner-demo/src/test/java/com/shreya/runner/ApplicationRunnerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationRunnerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
