ff258c1f21e4ebd7a3ebed654c9452e9eb4dd97b
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-20 18:15:47 +0530

    updated

--- application-runner-demo/src/main/java/com/shreya/runner/ApplicationRunnerDemoApplication.java
+++ application-runner-demo/src/main/java/com/shreya/runner/ApplicationRunnerDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.slf4j.Logger;
 
 @SpringBootApplication
