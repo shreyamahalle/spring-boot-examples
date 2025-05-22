a46f888285aef2d879bdc5757733de3287a94fc2
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-22 11:04:13 +0530

    i18 example (internationalization)

---
+++ i18-demo/src/main/java/com/shreya/practice/I18DemoApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18DemoApplication.class, args);
+	}
+
+}
---
+++ i18-demo/src/test/java/com/shreya/practice/I18DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
